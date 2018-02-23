82bb8795ccb22195f3a3fb3a31bdb4cc02edfb5f
dansbands <dansbands@hotmail.com>
2018-02-23 05:14:12 -0500

    insomnia

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
 import img from './IMG_0255_edited-1.png';
 import './App.css';
 
@@ -9,8 +8,99 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={img} className="App-logo" alt="logo" />
-          <h1 className="App-title">I am Stormy the Pitbull. This is MY app now.</h1>
+          <h1 className="App-title">I am Stormy the Pitbull. This is MY app now!</h1>
         </header>
+
+        <div className="row">
+          <div className="container">
+            <div  className="col-xs-4">
+
+              <div class="panel panel-default">
+                <div class="panel-heading">
+                  <h3 class="panel-title">Panel title</h3>
+                </div>
+                <div class="panel-body">
+                  Panel content
+                </div>
+              </div>
+              <div class="panel panel-default">
+                <div class="panel-heading">
+                  <h3 class="panel-title">Panel title</h3>
+                </div>
+                <div class="panel-body">
+                  Panel content
+                </div>
+              </div>
+              <div class="panel panel-default">
+                <div class="panel-heading">
+                  <h3 class="panel-title">Panel title</h3>
+                </div>
+                <div class="panel-body">
+                  Panel content
+                </div>
+              </div>
+              <div class="panel panel-default">
+                <div class="panel-heading">
+                  <h3 class="panel-title">Panel title</h3>
+                </div>
+                <div class="panel-body">
+                  Panel content
+                </div>
+              </div>
+              <div class="panel panel-default">
+                <div class="panel-heading">
+                  <h3 class="panel-title">Panel title</h3>
+                </div>
+                <div class="panel-body">
+                  Panel content
+                </div>
+              </div>
+              <div class="panel panel-default">
+                <div class="panel-heading">
+                  <h3 class="panel-title">Panel title</h3>
+                </div>
+                <div class="panel-body">
+                  Panel content
+                </div>
+              </div>
+              <div class="panel panel-default">
+                <div class="panel-heading">
+                  <h3 class="panel-title">Panel title</h3>
+                </div>
+                <div class="panel-body">
+                  Panel content
+                </div>
+              </div>
+              <div class="panel panel-default">
+                <div class="panel-heading">
+                  <h3 class="panel-title">Panel title</h3>
+                </div>
+                <div class="panel-body">
+                  Panel content
+                </div>
+              </div>
+
+            </div>
+            <div className="col-xs-8">
+              <h1>Goal Coach</h1>
+              <div className='form-group'>
+                <div className='form-group'>
+                  <label htmlFor="title">Title</label>
+                  <input type="text" id="title" className="form-control" placeholder="Your title here.."></input>
+                </div>
+              </div>
+              <form action="/action_page.php">
+                <div className="form-group">
+                  <label htmlFor="text">Body:</label>
+                  <textarea type="text" class="form-control" id="email" rows="8" col="80"/>
+                </div>
+                <button type="submit" class="btn btn-default">Submit</button>
+              </form>
+            </div>
+
+          </div>
+        </div>
+
       </div>
     );
   }
